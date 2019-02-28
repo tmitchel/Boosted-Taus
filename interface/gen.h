691c5223b39e2a14249c188368dcdbe8cd949668
Tyler Mitchell <tyler106@ksu.edu>
2019-02-28 11:45:32 -0600

    add getters for all mc data in ggNtuples to the Gen class

--- interface/gen.h
+++ interface/gen.h
@@ -8,28 +8,49 @@
 class Gen {
   friend class Gen_Factory;
  public:
-  Gen(Float_t, Float_t, Float_t, Float_t);
   Gen(Float_t, Float_t, Float_t, Float_t, bool);
 
   // getters
-  Int_t getPID() const { return pid; }
-  TLorentzVector getP4() { return p4; }
+  Int_t getPID() const { return PID; }
+  Int_t getMomPID() const { return MomPID; }
+  Int_t getGMomPID() const { return GMomPID; }
+  Int_t getStatus() const { return Status; }
+  Int_t getParentage() const { return Parentage; }
   Float_t getPt() const { return p4.Pt(); }
   Float_t getEta() const { return p4.Eta(); }
   Float_t getPhi() const { return p4.Phi(); }
   Float_t getMass() const { return p4.M(); }
+  Float_t getVtx() const { return Vtx; }
+  Float_t getVty() const { return Vty; }
+  Float_t getVtz() const { return Vtz; }
+  Float_t getCalIso04() const { return CalIsoDR04; }
+  Float_t getTrkIso03() const { return TrkIsoDR03; }
+  Float_t getTrkIso04() const { return TrkIsoDR04; }
+  Float_t getMomPt() const { return MomPt; }
+  Float_t getMomEta() const { return MomEta; }
+  Float_t getMomPhi() const { return MomPhi; }
+  Float_t getMomMass() const { return MomMass; }
+  TLorentzVector getP4() { return p4; }
+  TLorentzVector getMomP4();  // defined below
 
  private:
-  Int_t pid;
-  TLorentzVector p4;
+  TLorentzVector p4, MomP4;
+  Int_t PID, GMomPID, MomPID, Parentage, Status;
+  Float_t Vtx, Vty, Vtz, MomPt, MomMass, MomEta, MomPhi, CalIsoDR03, TrkIsoDR03, CalIsoDR04, TrkIsoDR04;
 };
 
-Gen::Gen(Float_t pt, Float_t eta, Float_t phi, Float_t mass) {
-  p4.SetPtEtaPhiM(pt, eta, phi, mass);
-}
 
 Gen::Gen(Float_t pt, Float_t eta, Float_t phi, Float_t energy, bool useE) {
-  p4.SetPtEtaPhiE(pt, eta, phi, energy);
+  if (useE) {
+    p4.SetPtEtaPhiE(pt, eta, phi, energy);
+  } else {
+    p4.SetPtEtaPhiM(pt, eta, phi, energy);
+  }
+}
+
+TLorentzVector Gen::getMomP4() {
+  MomP4.SetPtEtaPhiM(MomPt, MomEta, MomPhi, MomMass);
+  return MomP4;
 }
 
 #endif  // INTERFACE_GEN_H_
