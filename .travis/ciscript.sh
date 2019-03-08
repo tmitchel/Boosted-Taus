36fd0bbfb138719d37bee6e4b242119e243ac616
Tyler Mitchell <tyler106@ksu.edu>
2019-03-07 21:09:16 -0600

    add permissions to container

--- .travis/ciscript.sh
+++ .travis/ciscript.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-echo `ls`
-echo `pwd`
+
+
+# - Echo each command to stdout
+# - Stop on first command that fails
+set -ex
+
+# set permissions for output
+sudo chmod 777 /home/travis/build/tmitchel/Boosted-Taus/
+
 cd /home/travis/build/tmitchel/Boosted-Taus/
 ./build genStudy.cc a
