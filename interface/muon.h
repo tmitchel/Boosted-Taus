9f4dc60fd0a8f4450da1161a795fc94f4a84f224
Tyler Mitchell <tyler106@ksu.edu>
2019-03-18 10:21:37 -0500

    add the muon variables

--- interface/muon.h
+++ interface/muon.h
@@ -7,6 +7,7 @@
 
 class Muon {
   friend class Muon_Factory;
+
  public:
   Muon(Float_t, Float_t, Float_t, Float_t);
 
@@ -16,9 +17,43 @@ class Muon {
   Float_t getEta() { return p4.Eta(); }
   Float_t getPhi() { return p4.Phi(); }
   Float_t getMass() { return p4.M(); }
+  Float_t getFiredTrgs() { return FiredTrgs; }
+  Float_t getFiredL1Trgs() { return FiredL1Trgs; }
+  Float_t getCharge() { return Charge; }
+  Float_t getType() { return Type; }
+  Float_t getIDbit() { return IDbit; }
+  Float_t getTrkLayers() { return TrkLayers; }
+  Float_t getPixelLayers() { return PixelLayers; }
+  Float_t getPixelHits() { return PixelHits; }
+  Float_t getMuonHits() { return MuonHits; }
+  Float_t getStations() { return Stations; }
+  Float_t getMatches() { return Matches; }
+  Float_t getTrkQuality() { return TrkQuality; }
+  Float_t getBestTrkType() { return BestTrkType; }
+  Float_t getD0() { return D0; }
+  Float_t getDz() { return Dz; }
+  Float_t getSIP() { return SIP; }
+  Float_t getChi2NDF() { return Chi2NDF; }
+  Float_t getInnerD0() { return InnerD0; }
+  Float_t getInnerDz() { return InnerDz; }
+  Float_t getIsoTrk() { return IsoTrk; }
+  Float_t getPFChIso() { return PFChIso; }
+  Float_t getPFPhoIso() { return PFPhoIso; }
+  Float_t getPFNeuIso() { return PFNeuIso; }
+  Float_t getPFPUIso() { return PFPUIso; }
+  Float_t getInnervalidFraction() { return InnervalidFraction; }
+  Float_t getsegmentCompatibility() { return segmentCompatibility; }
+  Float_t getchi2LocalPosition() { return chi2LocalPosition; }
+  Float_t gettrkKink() { return trkKink; }
+  Float_t getBestTrkPtError() { return BestTrkPtError; }
+  Float_t getBestTrkPt() { return BestTrkPt; }
 
  private:
   TLorentzVector p4;
+  ULong64_t FiredTrgs, FiredL1Trgs;
+  Int_t Charge, Type, IDbit, TrkLayers, PixelLayers, PixelHits, MuonHits, Stations, Matches, TrkQuality, BestTrkType;
+  Float_t D0, Dz, SIP, Chi2NDF, InnerD0, InnerDz, IsoTrk, PFChIso, PFPhoIso, PFNeuIso, PFPUIso, InnervalidFraction, segmentCompatibility,
+      chi2LocalPosition, trkKink, BestTrkPtError, BestTrkPt;
 };
 
 Muon::Muon(Float_t pt, Float_t eta, Float_t phi, Float_t energy) {
