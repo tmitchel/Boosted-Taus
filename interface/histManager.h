2e339e550cb10e52c11b1b61b260de1d4a075a84
Tyler Mitchell <tyler106@ksu.edu>
2019-03-20 11:28:30 -0500

    add the histograms for trigger/tau eff

--- interface/histManager.h
+++ interface/histManager.h
@@ -25,18 +25,35 @@ class histManager {
 
 histManager::histManager(std::string fname) : fout(std::make_shared<TFile>(fname.c_str(), "RECREATE")) {
   fout->cd();
-  hists = {{"pt0_jet_flavor", new TH1F("pt0_jet_flavor", "pt0_jet_flavor", 7, 0.5, 7.5)},
-           {"pt400_jet_flavor", new TH1F("pt400_jet_flavor", "pt400_jet_flavor", 7, 0.5, 7.5)},
-           {"pt600_jet_flavor", new TH1F("pt600_jet_flavor", "pt600_jet_flavor", 7, 0.5, 7.5)},
-           {"pt800_jet_flavor", new TH1F("pt800_jet_flavor", "pt800_jet_flavor", 7, 0.5, 7.5)},
-           {"lead_jet_flavor", new TH1F("lead_jet_flavor", "lead_jet_flavor", 7, 0.5, 7.5)},
-           {"nevents", new TH1F("nevents", "nevents", 1, 0.5, 1.5)},
-           {"dr_taus", new TH1F("dr_taus", "dr_taus", 100, 0, 5)},
-           {"dr_jet_MET", new TH1F("dr_jet_MET", "dr_jet_MET", 100, 0, 5)},
-           {"dr_higgs_MET", new TH1F("dr_higgs_MET", "dr_higgs_MET", 100, 0, 5)},
-           {"dphi_taus", new TH1F("dphi_taus", "dphi_taus", 100, -3.14, 3.14)},
-           {"lead_gen_jet_eff", new TH1F("lead_gen_jet_eff", "lead_gen_jet_eff", 100, 0, 1000)},
-           {"lead_jet_eff", new TH1F("lead_jet_eff", "lead_jet_eff", 100, 0, 1000)}};
+  hists = {
+      {"pt0_jet_flavor", new TH1F("pt0_jet_flavor", "pt0_jet_flavor", 7, 0.5, 7.5)},
+      {"pt400_jet_flavor", new TH1F("pt400_jet_flavor", "pt400_jet_flavor", 7, 0.5, 7.5)},
+      {"pt600_jet_flavor", new TH1F("pt600_jet_flavor", "pt600_jet_flavor", 7, 0.5, 7.5)},
+      {"pt800_jet_flavor", new TH1F("pt800_jet_flavor", "pt800_jet_flavor", 7, 0.5, 7.5)},
+      {"lead_jet_flavor", new TH1F("lead_jet_flavor", "lead_jet_flavor", 7, 0.5, 7.5)},
+      {"nevents", new TH1F("nevents", "nevents", 1, 0.5, 1.5)},
+      {"dr_taus", new TH1F("dr_taus", "dr_taus", 100, 0, 5)},
+      {"dr_jet_MET", new TH1F("dr_jet_MET", "dr_jet_MET", 100, 0, 5)},
+      {"dr_higgs_MET", new TH1F("dr_higgs_MET", "dr_higgs_MET", 100, 0, 5)},
+      {"dphi_taus", new TH1F("dphi_taus", "dphi_taus", 100, -3.14, 3.14)},
+      {"lead_gen_jet_eff", new TH1F("lead_gen_jet_eff", "lead_gen_jet_eff", 100, 0, 1000)},
+      {"lead_jet_eff", new TH1F("lead_jet_eff", "lead_jet_eff", 100, 0, 1000)},
+      {"cutflow", new TH1F("cutflow", "cutflow", 3, 0.5, 3.5)},
+
+      {"is_prescaled", new TH1F("is_prescaled", "is_prescaled", 8, 0.5, 8.5)},
+      {"all_trigger_eff", new TH1F("all_trigger_eff", "all_trigger_eff", 4, 0.5, 4.5)},
+      {"jet320_trigger_eff", new TH1F("jet320_trigger_eff", "jet320_trigger_eff", 50, 0., 640)},
+      {"jet500_trigger_eff", new TH1F("jet500_trigger_eff", "jet500_trigger_eff", 50, 0., 1000)},
+      {"HT300_MET110_trigger_eff", new TH1F("HT300_MET110_trigger_eff", "HT300_MET110_trigger_eff", 50, 0., 1000.)},
+      {"HT800_trigger_eff", new TH1F("HT800_trigger_eff", "HT800_trigger_eff", 50, 0., 1600.)},
+
+      {"all_iso_eff", new TH1F("all_iso_eff", "all_iso_eff", 3, 0.5, 3.5)},
+      {"all_tau_pt", new TH1F("all_tau_pt", "all_tau_pt", 50, 0, 1000)},
+      {"ntau", new TH1F("ntau", "ntau", 1, 0.5, 1.5)},
+      {"loose_boosted_eff", new TH1F("loose_boosted_eff", "loose_boosted_eff", 50, 0., 600.)},
+      {"medium_boosted_eff", new TH1F("medium_boosted_eff", "medium_boosted_eff", 50, 0., 600.)},
+      {"tight_boosted_eff", new TH1F("tight_boosted_eff", "tight_boosted_eff", 50, 0., 600.)},
+  };
 }
 
 void histManager::Fill(std::string name, double var, double weight) { hists.at(name)->Fill(var, weight); }
--- recoStudy_v1.cc
+++ recoStudy_v1.cc
@@ -7,16 +7,17 @@
 #include "TLorentzVector.h"
 #include "TTree.h"
 #include "interface/CLParser.h"
+#include "interface/boosted_factory.h"
+#include "interface/event_factory.h"
 #include "interface/gen_factory.h"
 #include "interface/histManager.h"
-#include "interface/tau_factory.h"
-#include "interface/boosted_factory.h"
 #include "interface/jets_factory.h"
+#include "interface/tau_factory.h"
 
 using std::string;
 using std::vector;
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   auto parser = std::unique_ptr<CLParser>(new CLParser(argc, argv));
   auto verbose = parser->Flag("-v");
   auto input_name = parser->Option("-i");
@@ -26,24 +27,26 @@ int main(int argc, char** argv) {
   // read the input TFile/TTree
   auto fin = std::shared_ptr<TFile>(TFile::Open(input_name.c_str()));
   auto hists = std::make_shared<histManager>(output_name);
-  auto tree = reinterpret_cast<TTree*>(fin->Get(tree_name.c_str()));
+  auto tree = reinterpret_cast<TTree *>(fin->Get(tree_name.c_str()));
   // construct our object factories
   auto gen_factory = Gen_Factory(tree);
   auto tau_factory = Tau_Factory(tree);
   auto boost_factory = Boosted_Factory(tree);
   auto jet_factory = Jets_Factory(tree);
+  auto event = Event_Factory(tree);
 
   auto nevts = tree->GetEntries();
-  int progress(1), fraction((nevts-1)/10);
+  int progress(1), fraction((nevts - 1) / 10);
   for (auto i = 0; i < nevts; i++) {
     tree->GetEntry(i);
     if (i == progress * fraction && verbose) {
-      std::cout << "\t" << progress*10 << "% complete.\r" << std::flush;
+      std::cout << "\t" << progress * 10 << "% complete.\r" << std::flush;
       progress++;
     }
 
     // keep track of the number of events processed
     hists->Fill("nevents", 1., 1.);
+    hists->FillBin("cutflow", 1, 1.);
 
     // run all the factories to fill variables
     gen_factory.Run_Factory();
@@ -65,13 +68,78 @@ int main(int argc, char** argv) {
 
     // only look at events with high pT jet
     if (!found_jet) {
+      hists->Fill("cutflow", 2, 1.);
+    } else {
       continue;
     }
 
     // need to have at least 1 tau in the event
-    if (boost_factory.getNGoodBoosted() < 1) {
+    if (boost_factory.getNGoodBoosted() > 0) {
+      hists->Fill("cutflow", 3, 1.);
+    } else {
       continue;
     }
+
+    auto lead_tau = boost_factory.getTaus()->at(0);
+
+    /////////////////////
+    // trigger studies //
+    /////////////////////
+
+    // are triggers prescaled?
+    if (event.getJetTriggerIsPre(16)) {
+      hists->FillBin("is_prescaled", 1, 1.);
+    } else if (event.getJetTriggerIsPre(16)) {
+      hists->FillBin("is_prescaled", 2, 1.);
+    }
+
+    if (event.getJetTriggerIsPre(19)) {
+      hists->FillBin("is_prescaled", 3, 1.);
+    } else if (event.getJetTriggerIsPre(19)) {
+      hists->FillBin("is_prescaled", 4, 1.);
+    }
+
+    if (event.getJetTriggerIsPre(22)) {
+      hists->FillBin("is_prescaled", 5, 1.);
+    } else if (event.getJetTriggerIsPre(22)) {
+      hists->FillBin("is_prescaled", 6, 1.);
+    }
+
+    if (event.getJetTriggerIsPre(32)) {
+      hists->FillBin("is_prescaled", 7, 1.);
+    } else if (event.getJetTriggerIsPre(32)) {
+      hists->FillBin("is_prescaled", 8, 1.);
+    }
+
+    // trigger efficiencies
+
+    // HLT_PFJet320_v
+    if (event.getJetTrigger(16)) {
+      hists->FillBin("all_trigger_eff", 1, 1.);
+      hists->FillPrevBins("jet320_trigger_eff", lead_tau.getPt(), 1.);
+    }
+
+    // HLT_PFJet500_v
+    if (event.getJetTrigger(19)) {
+      hists->FillBin("all_trigger_eff", 2, 1.);
+      hists->FillPrevBins("jet500_trigger_eff", lead_tau.getPt(), 1.);
+    }
+
+    // HLT_PFHT300_PFMET110_v
+    if (event.getJetTrigger(22)) {
+      hists->FillBin("all_trigger_eff", 3, 1.);
+      hists->FillPrevBins("HT300_MET110_trigger_eff", lead_tau.getPt(), 1.);
+    }
+
+    // HLT_PFHT800_v
+    if (event.getJetTrigger(32)) {
+      hists->FillBin("all_trigger_eff", 4, 1.);
+      hists->FillPrevBins("HT800_trigger_eff", lead_tau.getPt(), 1.);
+    }
+
+    ////////////////////////////
+    // tau efficiency studies //
+    ////////////////////////////
 
     // find gen-level taus
     VGen all_gen_higgs, all_gen_taus, all_gen_jets;
@@ -85,16 +153,26 @@ int main(int argc, char** argv) {
     for (auto &tau : *boosts) {
       for (auto &gen : all_gen_taus) {
         if (tau.getP4().DeltaR(gen.getP4()) < 0.5) {
-          std::cout << "DENOMINATOR" << std::endl;
+          hists->FillBin("ntau", 1, 1.);
+          hists->FillPrevBins("all_tau_pt", tau.getPt(), 1.);
           if (tau.getIso(loose)) {
-            std::cout << "NUMERATOR" << std::endl;
+            hists->FillBin("all_iso_eff", 1, 1.);
+            hists->FillPrevBins("loose_boosted_eff", tau.getPt(), 1.);
+          }
+          if (tau.getIso(medium)) {
+            hists->FillBin("all_iso_eff", 2, 1.);
+            hists->FillPrevBins("medium_boosted_eff", tau.getPt(), 1.);
+          }
+          if (tau.getIso(tight)) {
+            hists->FillBin("all_iso_eff", 3, 1.);
+            hists->FillPrevBins("tight_boosted_eff", tau.getPt(), 1.);
           }
         }
       }
     }
 
-  std::cout << std::endl;
-  fin->Close();
-  hists->Write();
-  return 1;
-}
+    std::cout << std::endl;
+    fin->Close();
+    hists->Write();
+    return 1;
+  }
