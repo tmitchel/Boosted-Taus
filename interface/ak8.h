2db8756ff9e4b92fea41e13172e6fef62bf85c79
Tyler Mitchell <tyler106@ksu.edu>
2019-05-14 10:07:58 -0500

    inherit 4-momenta from base object

--- interface/ak8.h
+++ interface/ak8.h
@@ -3,73 +3,69 @@
 #ifndef INTERFACE_AK8_H_
 #define INTERFACE_AK8_H_
 
+#include "./object.h"
 #include "TLorentzVector.h"
 
-class AK8 {
-  friend class AK8_Factory;
- public:
-  AK8(Float_t, Float_t, Float_t, Float_t);
+class AK8 : public PObject {
+    friend class AK8_Factory;
 
-  // getters
-  TLorentzVector getP4() { return p4; }
-  Bool_t getJetPFLooseId() { return PFLooseId; }
-  Int_t getLepTrackPID() { return LepTrackPID; }
-  Int_t getPartonID() { return PartonID; }
-  Int_t getHadFlvr() { return HadFlvr; }
-  Int_t getID() { return ID; }
-  Int_t getNCH() { return NCH; }
-  Int_t getNNP() { return NNP; }
-  ULong64_t getjetFiredTrgs() { return jetFiredTrgs; }
-  Float_t getPrunedMass() { return PrunedMass; }
-  Float_t getSoftDropMass() { return SoftDropMass; }
-  Float_t getPt() { return p4.Pt(); }
-  Float_t getEta() { return p4.Eta(); }
-  Float_t getPhi() { return p4.Phi(); }
-  Float_t getMass() { return p4.M(); }
-  Float_t getRawPt() { return RawPt; }
-  Float_t getRawEn() { return RawEn; }
-  Float_t getMt() { return Mt; }
-  Float_t getArea() { return Area; }
-  Float_t getLeadTrackPt() { return LeadTrackPt; }
-  Float_t getLeadTrackEta() { return LeadTrackEta; }
-  Float_t getLeadTrackPhi() { return LeadTrackPhi; }
-  Float_t getLepTrackPt() { return LepTrackPt; }
-  Float_t getLepTrackEta() { return LepTrackEta; }
-  Float_t getLepTrackPhi() { return LepTrackPhi; }
-  Float_t getCSV2BJetTags() { return CSV2BJetTags; }
-  Float_t getDeepCSVTags_b() { return DeepCSVTags_b; }
-  Float_t getDeepCSVTags_bb() { return DeepCSVTags_bb; }
-  Float_t getDeepCSVTags_c() { return DeepCSVTags_c; }
-  Float_t getDeepCSVTags_udsg() { return DeepCSVTags_udsg; }
-  Float_t getP4Smear() { return P4Smear; }
-  Float_t getP4SmearUp() { return P4SmearUp; }
-  Float_t getP4SmearDo() { return P4SmearDo; }
-  Float_t getPUID() { return PUID; }
-  Float_t getJECUnc() { return JECUnc; }
-  Float_t getFiredTrgs() { return FiredTrgs; }
-  Float_t getCHF() { return CHF; }
-  Float_t getNHF() { return NHF; }
-  Float_t getCEF() { return CEF; }
-  Float_t getNEF() { return NEF; }
-  Float_t getMUF() { return MUF; }
-  Float_t getVtxPt() { return VtxPt; }
-  Float_t getVtxMass() { return VtxMass; }
-  Float_t getVtxNtrks() { return VtxNtrks; }
-  Float_t getVtx3DVal() { return Vtx3DVal; }
-  Float_t getVtx3DSig() { return Vtx3DSig; }
+   public:
+    AK8() : PObject() {}
+    AK8(Float_t, Float_t, Float_t, Float_t);
 
- private:
-  TLorentzVector p4;
-  Bool_t PFLooseId;
-  Int_t LepTrackPID, PartonID, HadFlvr, ID, PUFullID, NCH, NNP;
-  ULong64_t jetFiredTrgs;
-  Float_t RawPt, RawEn, Mt, Area, LeadTrackPt, LeadTrackEta, LeadTrackPhi, LepTrackPt, LepTrackEta, LepTrackPhi,
-      CSV2BJetTags, DeepCSVTags_b, DeepCSVTags_bb, DeepCSVTags_c, DeepCSVTags_udsg, P4Smear, P4SmearUp, P4SmearDo, PUID, JECUnc, FiredTrgs, CHF, NHF, CEF, NEF,
-      MUF, VtxPt, VtxMass, VtxNtrks, Vtx3DVal, Vtx3DSig, PrunedMass, SoftDropMass;
+    // getters
+    Bool_t getJetPFLooseId() { return PFLooseId; }
+    Int_t getLepTrackPID() { return LepTrackPID; }
+    Int_t getPartonID() { return PartonID; }
+    Int_t getHadFlvr() { return HadFlvr; }
+    Int_t getID() { return ID; }
+    Int_t getNCH() { return NCH; }
+    Int_t getNNP() { return NNP; }
+    ULong64_t getjetFiredTrgs() { return jetFiredTrgs; }
+    Float_t getPrunedMass() { return PrunedMass; }
+    Float_t getSoftDropMass() { return SoftDropMass; }
+    Float_t getRawPt() { return RawPt; }
+    Float_t getRawEn() { return RawEn; }
+    Float_t getMt() { return Mt; }
+    Float_t getArea() { return Area; }
+    Float_t getLeadTrackPt() { return LeadTrackPt; }
+    Float_t getLeadTrackEta() { return LeadTrackEta; }
+    Float_t getLeadTrackPhi() { return LeadTrackPhi; }
+    Float_t getLepTrackPt() { return LepTrackPt; }
+    Float_t getLepTrackEta() { return LepTrackEta; }
+    Float_t getLepTrackPhi() { return LepTrackPhi; }
+    Float_t getCSV2BJetTags() { return CSV2BJetTags; }
+    Float_t getDeepCSVTags_b() { return DeepCSVTags_b; }
+    Float_t getDeepCSVTags_bb() { return DeepCSVTags_bb; }
+    Float_t getDeepCSVTags_c() { return DeepCSVTags_c; }
+    Float_t getDeepCSVTags_udsg() { return DeepCSVTags_udsg; }
+    Float_t getP4Smear() { return P4Smear; }
+    Float_t getP4SmearUp() { return P4SmearUp; }
+    Float_t getP4SmearDo() { return P4SmearDo; }
+    Float_t getPUID() { return PUID; }
+    Float_t getJECUnc() { return JECUnc; }
+    Float_t getFiredTrgs() { return FiredTrgs; }
+    Float_t getCHF() { return CHF; }
+    Float_t getNHF() { return NHF; }
+    Float_t getCEF() { return CEF; }
+    Float_t getNEF() { return NEF; }
+    Float_t getMUF() { return MUF; }
+    Float_t getVtxPt() { return VtxPt; }
+    Float_t getVtxMass() { return VtxMass; }
+    Float_t getVtxNtrks() { return VtxNtrks; }
+    Float_t getVtx3DVal() { return Vtx3DVal; }
+    Float_t getVtx3DSig() { return Vtx3DSig; }
+
+   private:
+    Bool_t PFLooseId;
+    Int_t LepTrackPID, PartonID, HadFlvr, ID, PUFullID, NCH, NNP;
+    ULong64_t jetFiredTrgs;
+    Float_t RawPt, RawEn, Mt, Area, LeadTrackPt, LeadTrackEta, LeadTrackPhi, LepTrackPt, LepTrackEta, LepTrackPhi,
+        CSV2BJetTags, DeepCSVTags_b, DeepCSVTags_bb, DeepCSVTags_c, DeepCSVTags_udsg, P4Smear, P4SmearUp, P4SmearDo,
+        PUID, JECUnc, FiredTrgs, CHF, NHF, CEF, NEF, MUF, VtxPt, VtxMass, VtxNtrks, Vtx3DVal, Vtx3DSig, PrunedMass,
+        SoftDropMass;
 };
 
-AK8::AK8(Float_t pt, Float_t eta, Float_t phi, Float_t energy) {
-  p4.SetPtEtaPhiE(pt, eta, phi, energy);
-}
+AK8::AK8(Float_t pt, Float_t eta, Float_t phi, Float_t energy) : PObject(pt, eta, phi, energy, comp0::energy) {}
 
 #endif  // INTERFACE_AK8_H_
--- interface/tau.h
+++ interface/tau.h
@@ -4,127 +4,123 @@
 #define INTERFACE_TAU_H_
 
 #include <stdexcept>
+#include "./object.h"
 #include "./util.h"
 #include "TLorentzVector.h"
 
-class Tau {
-  friend class Tau_Factory;
+class Tau : public PObject {
+    friend class Tau_Factory;
 
- public:
-  Tau(Float_t, Float_t, Float_t, Float_t);
+   public:
+    Tau() : PObject() {}
+    Tau(Float_t, Float_t, Float_t, Float_t);
 
-  // getters
-  Bool_t getIso(working_point);
-  Bool_t getDiscByDM(bool);
-  Bool_t getEleRejection(working_point);
-  Bool_t getMuRejection(working_point);
-  Bool_t getCombinedIsolationDeltaBetaCorr3Hits(working_point);
-  Bool_t getLeadChargedHadronExists() { return LeadChargedHadronExists; }
-  Int_t getDecayMode() { return DecayMode; }
-  Int_t getNumSignalPFChargedHadrCands() { return NumSignalPFChargedHadrCands; }
-  Int_t getNumSignalPFNeutrHadrCands() { return NumSignalPFNeutrHadrCands; }
-  Int_t getNumSignalPFGammaCands() { return NumSignalPFGammaCands; }
-  Int_t getNumSignalPFCands() { return NumSignalPFCands; }
-  Int_t getNumIsolationPFChargedHadrCands() { return NumIsolationPFChargedHadrCands; }
-  Int_t getNumIsolationPFNeutrHadrCands() { return NumIsolationPFNeutrHadrCands; }
-  Int_t getNumIsolationPFGammaCands() { return NumIsolationPFGammaCands; }
-  Int_t getNumIsolationPFCands() { return NumIsolationPFCands; }
-  Float_t getPt() { return p4.Pt(); }
-  Float_t getEta() { return p4.Eta(); }
-  Float_t getPhi() { return p4.Phi(); }
-  Float_t getMass() { return p4.M(); }
-  Float_t getCombinedIsolationDeltaBetaCorrRaw3Hits() { return CombinedIsolationDeltaBetaCorrRaw3Hits; }
-  Float_t getCharge() { return Charge; }
-  Float_t getVz() { return Vz; }
-  Float_t getDxy() { return Dxy; }
-  Float_t getZImpact() { return ZImpact; }
-  Float_t getLeadChargedHadronEta() { return LeadChargedHadronEta; }
-  Float_t getLeadChargedHadronPhi() { return LeadChargedHadronPhi; }
-  Float_t getLeadChargedHadronPt() { return LeadChargedHadronPt; }
-  Float_t getChargedIsoPtSum() { return ChargedIsoPtSum; }
-  Float_t getNeutralIsoPtSum() { return NeutralIsoPtSum; }
-  Float_t getPuCorrPtSum() { return PuCorrPtSum; }
-  Float_t getfootprintCorrection() { return footprintCorrection; }
-  Float_t getphotonPtSumOutsideSignalCone() { return photonPtSumOutsideSignalCone; }
-  Float_t getDZ() { return dz; }
-  Float_t getDXY() { return dxy; }
-  TLorentzVector getP4() { return p4; }
+    // getters
+    Bool_t getIso(working_point);
+    Bool_t getDiscByDM(bool);
+    Bool_t getEleRejection(working_point);
+    Bool_t getMuRejection(working_point);
+    Bool_t getCombinedIsolationDeltaBetaCorr3Hits(working_point);
+    Bool_t getLeadChargedHadronExists() { return LeadChargedHadronExists; }
+    Int_t getDecayMode() { return DecayMode; }
+    Int_t getNumSignalPFChargedHadrCands() { return NumSignalPFChargedHadrCands; }
+    Int_t getNumSignalPFNeutrHadrCands() { return NumSignalPFNeutrHadrCands; }
+    Int_t getNumSignalPFGammaCands() { return NumSignalPFGammaCands; }
+    Int_t getNumSignalPFCands() { return NumSignalPFCands; }
+    Int_t getNumIsolationPFChargedHadrCands() { return NumIsolationPFChargedHadrCands; }
+    Int_t getNumIsolationPFNeutrHadrCands() { return NumIsolationPFNeutrHadrCands; }
+    Int_t getNumIsolationPFGammaCands() { return NumIsolationPFGammaCands; }
+    Int_t getNumIsolationPFCands() { return NumIsolationPFCands; }
+    Float_t getCombinedIsolationDeltaBetaCorrRaw3Hits() { return CombinedIsolationDeltaBetaCorrRaw3Hits; }
+    Float_t getCharge() { return Charge; }
+    Float_t getVz() { return Vz; }
+    Float_t getDxy() { return Dxy; }
+    Float_t getZImpact() { return ZImpact; }
+    Float_t getLeadChargedHadronEta() { return LeadChargedHadronEta; }
+    Float_t getLeadChargedHadronPhi() { return LeadChargedHadronPhi; }
+    Float_t getLeadChargedHadronPt() { return LeadChargedHadronPt; }
+    Float_t getChargedIsoPtSum() { return ChargedIsoPtSum; }
+    Float_t getNeutralIsoPtSum() { return NeutralIsoPtSum; }
+    Float_t getPuCorrPtSum() { return PuCorrPtSum; }
+    Float_t getfootprintCorrection() { return footprintCorrection; }
+    Float_t getphotonPtSumOutsideSignalCone() { return photonPtSumOutsideSignalCone; }
+    Float_t getDZ() { return dz; }
+    Float_t getDXY() { return dxy; }
 
- private:
-  TLorentzVector p4;
-  Bool_t pass_vloose_iso, pass_loose_iso, pass_medium_iso, pass_tight_iso, pass_vtight_iso;
+   private:
+    Bool_t pass_vloose_iso, pass_loose_iso, pass_medium_iso, pass_tight_iso, pass_vtight_iso;
 
-  Bool_t pfTausDiscriminationByDecayModeFinding, pfTausDiscriminationByDecayModeFindingNewDMs, ByMVA6VLooseElectronRejection, ByMVA6LooseElectronRejection,
-      ByMVA6MediumElectronRejection, ByMVA6TightElectronRejection, ByMVA6VTightElectronRejection, ByLooseMuonRejection3, ByTightMuonRejection3,
-      ByLooseCombinedIsolationDeltaBetaCorr3Hits, ByMediumCombinedIsolationDeltaBetaCorr3Hits, ByTightCombinedIsolationDeltaBetaCorr3Hits, LeadChargedHadronExists;
+    Bool_t pfTausDiscriminationByDecayModeFinding, pfTausDiscriminationByDecayModeFindingNewDMs,
+        ByMVA6VLooseElectronRejection, ByMVA6LooseElectronRejection, ByMVA6MediumElectronRejection,
+        ByMVA6TightElectronRejection, ByMVA6VTightElectronRejection, ByLooseMuonRejection3, ByTightMuonRejection3,
+        ByLooseCombinedIsolationDeltaBetaCorr3Hits, ByMediumCombinedIsolationDeltaBetaCorr3Hits,
+        ByTightCombinedIsolationDeltaBetaCorr3Hits, LeadChargedHadronExists;
 
-  Int_t DecayMode, NumSignalPFChargedHadrCands, NumSignalPFNeutrHadrCands, NumSignalPFGammaCands, NumSignalPFCands, NumIsolationPFChargedHadrCands,
-      NumIsolationPFNeutrHadrCands, NumIsolationPFGammaCands, NumIsolationPFCands;
+    Int_t DecayMode, NumSignalPFChargedHadrCands, NumSignalPFNeutrHadrCands, NumSignalPFGammaCands, NumSignalPFCands,
+        NumIsolationPFChargedHadrCands, NumIsolationPFNeutrHadrCands, NumIsolationPFGammaCands, NumIsolationPFCands;
 
-  Float_t CombinedIsolationDeltaBetaCorrRaw3Hits, Charge, Vz, Dxy, Mass,
-      ZImpact, LeadChargedHadronEta, LeadChargedHadronPhi, LeadChargedHadronPt, ChargedIsoPtSum, NeutralIsoPtSum, PuCorrPtSum, footprintCorrection,
-      photonPtSumOutsideSignalCone, dz, dxy, Energy;
+    Float_t CombinedIsolationDeltaBetaCorrRaw3Hits, Charge, Vz, Dxy, Mass, ZImpact, LeadChargedHadronEta,
+        LeadChargedHadronPhi, LeadChargedHadronPt, ChargedIsoPtSum, NeutralIsoPtSum, PuCorrPtSum, footprintCorrection,
+        photonPtSumOutsideSignalCone, dz, dxy, Energy;
 };
 
-Tau::Tau(Float_t pt, Float_t eta, Float_t phi, Float_t mass = 1.77682) {
-  p4.SetPtEtaPhiM(pt, eta, phi, mass);
-}
+Tau::Tau(Float_t pt, Float_t eta, Float_t phi, Float_t mass = 1.77682) : PObject(pt, eta, phi, mass, comp0::mass) {}
 
 Bool_t Tau::getIso(working_point wp) {
-  if (wp == 0) {
-    return pass_vloose_iso;
-  } else if (wp == 1) {
-    return pass_loose_iso;
-  } else if (wp == 2) {
-    return pass_medium_iso;
-  } else if (wp == 3) {
-    return pass_tight_iso;
-  } else if (wp == 4) {
-    return pass_vtight_iso;
-  }
-  throw std::invalid_argument("Must use one of the provided tau isolation WPs");
+    if (wp == 0) {
+        return pass_vloose_iso;
+    } else if (wp == 1) {
+        return pass_loose_iso;
+    } else if (wp == 2) {
+        return pass_medium_iso;
+    } else if (wp == 3) {
+        return pass_tight_iso;
+    } else if (wp == 4) {
+        return pass_vtight_iso;
+    }
+    throw std::invalid_argument("Must use one of the provided tau isolation WPs");
 }
 
 Bool_t Tau::getDiscByDM(bool newDM = false) {
-  if (newDM) {
-    return pfTausDiscriminationByDecayModeFindingNewDMs;
-  }
-  return pfTausDiscriminationByDecayModeFinding;
+    if (newDM) {
+        return pfTausDiscriminationByDecayModeFindingNewDMs;
+    }
+    return pfTausDiscriminationByDecayModeFinding;
 }
 
 Bool_t Tau::getEleRejection(working_point wp) {
-  if (wp == 0) {
-    return ByMVA6VLooseElectronRejection;
-  } else if (wp == 1) {
-    return ByMVA6LooseElectronRejection;
-  } else if (wp == 2) {
-    return ByMVA6MediumElectronRejection;
-  } else if (wp == 3) {
-    return ByMVA6TightElectronRejection;
-  } else if (wp == 4) {
-    return ByMVA6VTightElectronRejection;
-  }
-  throw std::invalid_argument("Must use one of the provided electron rejection WPs");
+    if (wp == 0) {
+        return ByMVA6VLooseElectronRejection;
+    } else if (wp == 1) {
+        return ByMVA6LooseElectronRejection;
+    } else if (wp == 2) {
+        return ByMVA6MediumElectronRejection;
+    } else if (wp == 3) {
+        return ByMVA6TightElectronRejection;
+    } else if (wp == 4) {
+        return ByMVA6VTightElectronRejection;
+    }
+    throw std::invalid_argument("Must use one of the provided electron rejection WPs");
 }
 
 Bool_t Tau::getMuRejection(working_point wp) {
-  if (wp == 1) {
-    return ByLooseMuonRejection3;
-  } else if (wp == 3) {
-    return ByTightMuonRejection3;
-  }
-  throw std::invalid_argument("Muon rejection working point was neither Tight nor Loose");
+    if (wp == 1) {
+        return ByLooseMuonRejection3;
+    } else if (wp == 3) {
+        return ByTightMuonRejection3;
+    }
+    throw std::invalid_argument("Muon rejection working point was neither Tight nor Loose");
 }
 
 Bool_t Tau::getCombinedIsolationDeltaBetaCorr3Hits(working_point wp) {
-  if (wp == 1) {
-    return ByLooseCombinedIsolationDeltaBetaCorr3Hits;
-  } else if (wp == 2) {
-    return ByMediumCombinedIsolationDeltaBetaCorr3Hits;
-  } else if (wp == 3) {
-    return ByTightCombinedIsolationDeltaBetaCorr3Hits;
-  }
-  throw std::invalid_argument("Must use one of the provided tau isolation WPs");
+    if (wp == 1) {
+        return ByLooseCombinedIsolationDeltaBetaCorr3Hits;
+    } else if (wp == 2) {
+        return ByMediumCombinedIsolationDeltaBetaCorr3Hits;
+    } else if (wp == 3) {
+        return ByTightCombinedIsolationDeltaBetaCorr3Hits;
+    }
+    throw std::invalid_argument("Must use one of the provided tau isolation WPs");
 }
 
 #endif  // INTERFACE_TAU_H_
