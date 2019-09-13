a7c5cd75967f2c2157267480dd03e0e6acf8447f
Tyler Mitchell <tyler106@ksu.edu>
2019-09-13 13:58:41 -0500

    add control region

--- datacard-builder.py
+++ datacard-builder.py
@@ -49,6 +49,11 @@ def get_histos(fout, fileList, variable, rebin=None):
         'ss_antiid_iso': ss_antiid_iso,
         'os_id_antiiso': os_id_antiiso,
         'ss_id_antiiso': ss_id_antiiso,
+        'os_id_ctr': os_id_ctr,
+        'ss_id_ctr': ss_id_ctr,
+        'os_antiid_ctr': os_antiid_ctr,
+        'ss_antiid_ctr': ss_antiid_ctr,
+
     }
 
 
@@ -62,6 +67,10 @@ def build_qcd(osss_histos):
     ss_antiid_iso = osss_histos['ss_antiid_iso']['Data'].Clone()
     os_id_antiiso = osss_histos['os_id_antiiso']['Data'].Clone()
     ss_id_antiiso = osss_histos['ss_id_antiiso']['Data'].Clone()
+    os_id_ctr = osss_histos['os_id_ctr']['Data'].Clone()
+    ss_id_ctr = osss_histos['ss_id_ctr']['Data'].Clone()
+    os_antiid_ctr = osss_histos['os_antiid_ctr']['Data'].Clone()
+    ss_antiid_ctr = osss_histos['ss_antiid_ctr']['Data'].Clone()
 
     # do data - bkgs
     bkgs = ['DYJets125', 'SingleTop', 'ttbar', 'WJets', 'Diboson']
@@ -74,21 +83,30 @@ def build_qcd(osss_histos):
         ss_antiid_iso.Add(osss_histos['ss_antiid_iso'][bkg], -1)
         os_id_antiiso.Add(osss_histos['os_id_antiiso'][bkg], -1)
         ss_id_antiiso.Add(osss_histos['ss_id_antiiso'][bkg], -1)
+        os_id_ctr.Add(osss_histos['os_id_ctr'][bkg], -1)
+        ss_id_ctr.Add(osss_histos['ss_id_ctr'][bkg], -1)
+        os_antiid_ctr.Add(osss_histos['os_antiid_ctr'][bkg], -1)
+        ss_antiid_ctr.Add(osss_histos['ss_antiid_ctr'][bkg], -1)
 
     nom_osss_ratio = os_antiid_iso.Integral() / ss_antiid_iso.Integral()
     antiiso_osss_ratio = os_antiid_antiiso.Integral() / ss_antiid_antiiso.Integral()
+    control_osss_ratio = os_antiid_ctr.Integral() / ss_antiid_ctr.Integral()
 
     nom_norm = ss_id_iso.Clone()
     antiiso_norm = ss_id_antiiso.Clone()
+    control_norm = ss_id_ctr.Clone()
 
     nom_norm.Scale(nom_osss_ratio)
     antiiso_norm.Scale(antiiso_osss_ratio)
+    control_norm.Scale(control_osss_rati()
 
     nom_qcd = ss_id_antiiso.Clone()
     antiiso_qcd = ss_id_iso.Clone()
+    control_qcd = ss_id_ctr.Clone9)
 
     nom_qcd.Scale(nom_norm.Integral() / nom_qcd.Integral())
     antiiso_qcd.Scale(antiiso_norm.Integral() / antiiso_qcd.Integral())
+    control_qcd.Scale(control_norm.Integral() / control_qcd.Integral())
 
     for ibin in range(0, nom_qcd.GetNbinsX() + 1):
         if nom_qcd.GetBinContent(ibin) < 0:
@@ -97,7 +115,10 @@ def build_qcd(osss_histos):
         if antiiso_qcd.GetBinContent(ibin) < 0:
             antiiso_qcd.SetBinContent(ibin, 0)
 
-    return nom_qcd, antiiso_qcd
+        if control_qcd.GetBinContent(ibin) < 0:
+            control_qcd.SetBinContent(ibin, 0)
+
+    return nom_qcd, antiiso_qcd, control_qcd
 
 
 def main(args):
@@ -105,7 +126,9 @@ def main(args):
     filelist = [ifile for ifile in glob('{}/*.root'.format(args.input_dir))]
     fout = ROOT.TFile('out.root', 'RECREATE')
     histos, osss_histos = get_histos(fout, filelist, '')
-    histos['QCD'], osss_histos['os_id_antiiso']['QCD'] = build_qcd(osss_histos)
+    histos['QCD'], osss_histos['os_id_antiiso']['QCD'], osss_histos['os_id_control']['QCD'] = build_qcd(osss_histos)
+
+    # pass region
     fout.mkdir('pass')
     fout.cd('pass')
     data = histos.pop('Data')
@@ -115,12 +138,23 @@ def main(args):
         histo.SetName(name)
         histo.Write()
 
+    # fail region
     fout.mkdir('fail')
     fout.cd('fail')
     antiiso_data = osss_histos['os_id_antiiso'].pop('Data')
     antiiso_data.SetName('data_obs')
     antiiso_data.Write()
     for name, histo in osss_histos['os_id_antiiso'].iteritems():
+        histo.SetName(name)
+        histo.Write()
+
+    # Zmumu control region
+    fout.mkdir('control')
+    fout.cd('control')
+    antiiso_data = osss_histos['os_id_control'].pop('Data')
+    antiiso_data.SetName('data_obs')
+    antiiso_data.Write()
+    for name, histo in osss_histos['os_id_control'].iteritems():
         histo.SetName(name)
         histo.Write()
 
