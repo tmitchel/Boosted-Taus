459108cc816e40facded05e8a15ecac27a3e4551
Tyler Mitchell <tyler106@ksu.edu>
2019-11-04 13:58:05 -0600

    don't need context, just need to add shell to call

--- scripts/tree_hadder.py
+++ scripts/tree_hadder.py
@@ -1,6 +1,5 @@
 from subprocess import call
 from glob import glob
-import context
 
 files = {
     'DataEl': ['SingleElectron*'],
@@ -14,31 +13,30 @@ files = {
 
 
 def main(args):
-    with context.cd('../'): # move out of ./scripts into base directory
-        # move original outputs to a new directory
-        call('mkdir {}/originals'.format(args.input))
-        call('mv {0}/*.root {0}/originals'.format(args.input))
+    # move original outputs to a new directory
+    call('mkdir {}/originals'.format(args.input), shell=True)
+    call('mv {0}/*.root {0}/originals'.format(args.input), shell=True)
 
-        # move logs into a new directory
-        call('mkdir {}/logs'.format(args.input))
-        call('mv {0}/*.log {0}/logs'.format(args.input))
+    # move logs into a new directory
+    call('mkdir {}/logs'.format(args.input), shell=True)
+    call('mv {0}/*.log {0}/logs'.format(args.input), shell=True)
 
-        for name, pattern in files.iteritems():
-            # append directory path to glob
-            pattern = ['{}/originals/{}'.format(args.input, p) for p in pattern]
+    for name, pattern in files.iteritems():
+        # append directory path to glob
+        pattern = ['{}/originals/{}'.format(args.input, p) for p in pattern]
 
-            # check if any files exist to be hadded
-            matches = 0
-            for p in pattern:
-                matches += len(glob(p))
+        # check if any files exist to be hadded
+        matches = 0
+        for p in pattern:
+            matches += len(glob(p))
 
-            # skip if none are found
-            if matches == 0:
-                print '\033[93m[WARNING] No files to hadd for {}. Skipping...\033[0m'.format(name)
-                continue
+        # skip if none are found
+        if matches == 0:
+            print '\033[93m[WARNING] No files to hadd for {}. Skipping...\033[0m'.format(name)
+            continue
 
-            # if there are files to hadd, do it
-            call('hadd {}/{}.root {}'.format(args.input, name, ' '.join(pattern)))
+        # if there are files to hadd, do it
+        call('hadd {}/{}.root {}'.format(args.input, name, ' '.join(pattern)), shell=True)
 
 
 if __name__ == "__main__":
